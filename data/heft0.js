f06462d2b9fd25604e16a73d646cdb578752258e
snailsstons <mrdrag.666@gmail.com>
2026-03-27 15:51:43 +0100

    Add files via upload

---
+++ data/heft0.js
@@ -0,0 +1,40 @@
+hefte["0"] = {
+  "titel": "Schmierheft",
+  "released": true,
+  "cover": "cover.jpg",
+  "video": "",
+  "teaser": "Experimente. Kein Plan. Einfach ausprobieren.",
+  "locDna": "",
+  "coverStory": "",
+  "seiten": [
+    {
+      "nummer": 1,
+      "panels": [
+        {
+          "bild": "0_1_P1.jpg",
+          "wide": true,
+          "tall": false,
+          "format": "16:9",
+          "locDna": "",
+          "story": "",
+          "skript": "",
+          "caption": ""
+        },
+        {
+          "bild": "0_1_P2.jpg",
+          "locDna": "",
+          "story": "",
+          "skript": "",
+          "caption": ""
+        },
+        {
+          "bild": "0_1_P3.jpg",
+          "locDna": "",
+          "story": "",
+          "skript": "",
+          "caption": ""
+        }
+      ]
+    }
+  ]
+};
