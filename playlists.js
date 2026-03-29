80aab181727138656ba9603180ac483d1796c9db
snailsstons <mrdrag.666@gmail.com>
2026-03-29 18:38:57 +0200

    Admin: Update playlists.js

--- playlists.js
+++ playlists.js
@@ -1,8 +1,40 @@
 window.playlistMap = [
-  { "titel": "Gandia und Umgebung", "hefte": [18, 19, 20] },
-  { "titel": "Die Borgia Route", "hefte": [] },
-  { "titel": "Valencia Vibes", "hefte": [15] },
-  { "titel": "Shorts", "hefte": [23, 24, 25] },
-  { "titel": "🖇️ Experimente, Schmierheft", "hefte": [0] },
-  { "titel": "⭐ Members Only", "hefte": [], "membersOnly": true }
+  {
+    "titel": "Gandia und Umgebung",
+    "hefte": [
+      18,
+      19,
+      20
+    ]
+  },
+  {
+    "titel": "Die Borgia Route",
+    "hefte": []
+  },
+  {
+    "titel": "Valencia Vibes",
+    "hefte": [
+      15
+    ]
+  },
+  {
+    "titel": "Shorts",
+    "hefte": [
+      23,
+      24,
+      25
+    ]
+  },
+  {
+    "titel": "🖇️ Experimente, Schmierheft",
+    "hefte": [
+      0,
+      25
+    ]
+  },
+  {
+    "titel": "⭐ Members Only",
+    "hefte": [],
+    "membersOnly": true
+  }
 ];
