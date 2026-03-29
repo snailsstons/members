1521d9bcee8142341e79878594042826d531f15b
snailsstons <mrdrag.666@gmail.com>
2026-03-29 18:30:46 +0200

    Add files via upload

---
+++ playlists.js
@@ -0,0 +1,8 @@
+window.playlistMap = [
+  { "titel": "Gandia und Umgebung", "hefte": [18, 19, 20] },
+  { "titel": "Die Borgia Route", "hefte": [] },
+  { "titel": "Valencia Vibes", "hefte": [15] },
+  { "titel": "Shorts", "hefte": [23, 24, 25] },
+  { "titel": "🖇️ Experimente, Schmierheft", "hefte": [0] },
+  { "titel": "⭐ Members Only", "hefte": [], "membersOnly": true }
+];
