01f42b5630ee9e1a6939cc9fe68724ff6668140b
snailsstons <mrdrag.666@gmail.com>
2026-03-29 16:49:38 +0200

    Admin: Update heft24.js

--- data/heft24.js
+++ data/heft24.js
@@ -1,7 +1,7 @@
 hefte["24"] = {
   "titel": "Die Flotte des Polit",
   "teaser": "Im Hafen von Gandia versteckt sich eine Schatzkammer aus Holz und Leidenschaft – ein Museum voller handgebauter Schiffe. Felix will jedes Modell fotografieren, Archie doziert über die Geschichte des Hafens, Stella findet die Ästhetik der alten Schiffe zeitlos – und Luna spürt die Hingabe eines Mannes, der hier sein Lebenswerk schuf. Am Ende, fast unbemerkt, ein Blick in die Ferne.",
-  "released": true,
+  "released": false,
   "cover": "cover.jpg",
   "seiten": [
     {
